4e1734c43cbd8cd18dc2526fd4a7d064876bb43b
Brendan Hansknecht <Brendan.Hansknecht@gmail.com>
2017-03-26 14:28:35 -0400

    Scheduler now calculates prereq chain

--- Scheduler/scheduler.py
+++ Scheduler/scheduler.py
@@ -3,15 +3,22 @@ import MySQLdb
 import re
 
 
-def check_prereqs(input, prereqLength):
-    input = input.replace("(C)", "")
+# would normal grab degree information here but instead I am faking it for testing
+degree = "CS"
+courses = ['CS 1000', 'CS 1121', 'CS 1122', 'CS 1142', 'CS 2311', 'CS 2321', 'CS 3000', 'CS 3141', 'CS 3311', 'CS 3331',
+           'CS 3411', 'CS 3421', 'CS 3425', 'CS 4121', 'CS 4321', 'MA 1160', 'MA 2160', 'MA 3710', 'MA 2330', 'UN 1025',
+           'UN 1015']
+complete = ['CS 1000', 'CS 1121', 'MA 1160', 'UN 1015']
 
+
+def check_prereqs(input, prereqLength, prereqChain):
+    input = input.replace("(C)", "")
     # deal with parentheses
     exp = re.findall("\(([^)]+)\)", input)
     if exp:
         for group in exp:
             group_with_parentheses = "(" + group + ")"
-            input = input.replace(group_with_parentheses, str(check_prereqs(group, prereqLength)))
+            input = input.replace(group_with_parentheses, str(check_prereqs(group, prereqLength, prereqChain)))
             # print(str)
 
     # deal with 'and's
@@ -21,18 +28,27 @@ def check_prereqs(input, prereqLength):
         # inside of ands deal with 'or's
         t = group.split(" or ")
         min = math.nan
+        minPrereq = "null"
         for prereq in t:
             # or logic
             if prereq in prereqLength:
                 if not math.isnan(prereqLength[prereq]) and math.isnan(min):
                     min = prereqLength[prereq]
+                    minPrereq = prereq
                 elif not math.isnan(prereqLength[prereq]) and prereqLength[prereq] < min:
                     min = prereqLength[prereq]
+                    minPrereq = prereq
             elif prereq.isdigit():
                 if math.isnan(min):
                     min = int(prereq)
+                    minPrereq = "null"
                 elif int(prereq) < min:
-                    min = prereqLength[prereq]
+                    min = int(prereq)
+                    minPrereq = "null"
+
+        if minPrereq != "null" and minPrereq not in prereqChain:
+            prereqChain.append(minPrereq)
+
         # and logic
         if math.isnan(min):
             value = math.nan
@@ -43,16 +59,11 @@ def check_prereqs(input, prereqLength):
             value = min
     return value
 
-
-# would normal grab degree information here but instead I am faking it for testing
-degree = "CS"
-courses = ['CS 1000', 'CS 1121', 'CS 1122', 'CS 1142', 'CS 2311', 'CS 2321', 'CS 3000', 'CS 3141', 'CS 3311', 'CS 3331',
-           'CS 3411', 'CS 3421', 'CS 3425', 'CS 4121', 'CS 4321', 'MA 1160', 'MA 2160', 'MA 3710', 'MA 2330', 'UN 1025']
-complete = ['CS 1000', 'CS 1121', 'MA 1160']
-
 prereqLength = {}
+prereqChain = {}
 for course in courses:
     prereqLength[course] = math.nan
+    prereqChain[course] = []
 
 db = MySQLdb.connect("141.219.214.79", "degreePlanner", "teamsoftware", "TSP")
 
@@ -66,7 +77,7 @@ for course in courses:
     data = cursor.fetchall()
     prereqs[data[0][0]] = data[0][1]
     if data[0][1] == 'null' or course in complete:
-        prereqLength[course] = 0
+        prereqLength[course] = -1
 db.close()
 
 change = True
@@ -74,11 +85,10 @@ while change:
     change = False
     for key, value in prereqLength.items():
         if math.isnan(prereqLength[key]):
-            val = check_prereqs(prereqs[key], prereqLength)
+            val = check_prereqs(prereqs[key], prereqLength, prereqChain[key])
             if not math.isnan(val):
                 change = True
                 prereqLength[key] = int(val) + 1
-
         """
         if math.isnan(value):
             #if prereq met then set it to highest prereq plus 1
@@ -123,8 +133,27 @@ while change:
             #mix of 'and's and 'or's
 
 """
-for key, value in prereqLength.items():
-    if math.isnan(prereqLength[key]):
-        print(key, ": ", prereqs[key])
-    else:
-        print(key, ": ", value)
+change = True
+while change:
+    change = False
+    for key, value in prereqChain.items():
+        for course in value:
+            if course in prereqChain.keys():
+                for prereq in prereqChain[course]:
+                    if prereq not in prereqChain[key]:
+                        change = True
+                        prereqChain[key].append(prereq)
+
+maxPrereqChain = 0
+for value in prereqLength.values():
+    if value > maxPrereqChain:
+        maxPrereqChain = value
+
+for i in range(maxPrereqChain + 1):
+    i = maxPrereqChain - i
+    for key, value in prereqLength.items():
+        if value == i:
+            if key not in complete:
+                print(key, ": ", value)
+                for v in prereqChain[key]:
+                    print("   ", v)
