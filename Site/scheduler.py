0898b1d37d75c2449d130608633b35940399a02a
Brendan Hansknecht <Brendan.Hansknecht@gmail.com>
2017-04-07 21:25:06 -0400

    Scheduler uses username as optional input

--- Site/scheduler.py
+++ Site/scheduler.py
@@ -1,14 +1,32 @@
 import math
 import MySQLdb
 import re
+import sys
 
 
 # would normal grab degree information here but instead I am faking it for testing
-degree = "CS"
+degree = "Computer Science"
 courses = ['CS 1000', 'CS 1121', 'CS 1122', 'CS 1142', 'CS 2311', 'CS 2321', 'CS 3000', 'CS 3141', 'CS 3311', 'CS 3331',
            'CS 3411', 'CS 3421', 'CS 3425', 'CS 4121', 'CS 4321', 'MA 1160', 'MA 2160', 'MA 3710', 'MA 2330', 'UN 1025',
            'UN 1015']
-complete = ['CS 1000', 'CS 1121', 'MA 1160', 'UN 1015']
+complete = []
+
+if len(sys.argv) > 1:
+    #print("loading info for user:", sys.argv[1])
+    db = MySQLdb.connect("141.219.214.79", "degreePlanner", "teamsoftware", "TSP")
+    cursor = db.cursor()
+    query = "SELECT * from profiles WHERE username='" + sys.argv[1] + "'"
+    cursor.execute(query)
+    data = cursor.fetchone()
+    if data[3] != '':
+        degree = data[3]
+    complete = data[5].split(", ")
+    query = "SELECT * from majors WHERE major='" + degree + "'"
+    cursor.execute(query)
+    data = cursor.fetchone()
+    courses = data[1].split(", ")
+    db.close()
+semesters = []
 
 def is_digit(n):
     try:
@@ -69,62 +87,82 @@ def check_prereqs(input, prereqLength, prereqChain):
             value = min
     return value
 
-prereqLength = {}
-prereqChain = {}
-for course in courses:
-    prereqLength[course] = math.nan
-    prereqChain[course] = []
+while set(courses) != set(complete):
+    #print(set(courses))
+    #print(set(complete))
+    prereqLength = {}
+    prereqChain = {}
+    for course in courses:
+        prereqLength[course] = math.nan
+        prereqChain[course] = []
 
-db = MySQLdb.connect("141.219.214.79", "degreePlanner", "teamsoftware", "TSP")
+    db = MySQLdb.connect("141.219.214.79", "degreePlanner", "teamsoftware", "TSP")
 
-cursor = db.cursor()
+    cursor = db.cursor()
 
-prereqs = {}
-# get prereqs of every course
-for course in courses:
-    query = "SELECT course_num, prereq from courses WHERE course_num='" + course + "'"
-    cursor.execute(query)
-    data = cursor.fetchall()
-    prereqs[data[0][0]] = data[0][1]
-    if data[0][1] == 'null' or course in complete:
-        prereqLength[course] = -1
-db.close()
+    prereqs = {}
+    # get prereqs of every course
+    for course in courses:
+        query = "SELECT course_num, prereq from courses WHERE course_num='" + course + "'"
+        cursor.execute(query)
+        data = cursor.fetchall()
+        prereqs[data[0][0]] = data[0][1]
+        if data[0][1] == 'null':
+            prereqLength[course] = 0
+        if course in complete:
+            prereqLength[course] = -1
+    db.close()
 
-change = True
-while change:
-    change = False
-    for key, value in prereqLength.items():
-        if math.isnan(prereqLength[key]):
-            val = check_prereqs(prereqs[key], prereqLength, prereqChain[key])
-            if not math.isnan(val):
-                change = True
-                prereqLength[key] = int(val) + 1
+    change = True
+    while change:
+        change = False
+        for key, value in prereqLength.items():
+            if math.isnan(prereqLength[key]):
+                val = check_prereqs(prereqs[key], prereqLength, prereqChain[key])
+                if not math.isnan(val):
+                    change = True
+                    prereqLength[key] = int(val) + 1
 
-change = True
-while change:
-    change = False
+    change = True
+    while change:
+        change = False
+        for key, value in prereqChain.items():
+            for course in value:
+                if course in prereqChain.keys():
+                    for prereq in prereqChain[course]:
+                        if prereq not in prereqChain[key]:
+                            change = True
+                            prereqChain[key].append(prereq)
+
+    maxPrereqChain = 0
+    for value in prereqLength.values():
+        if value > maxPrereqChain:
+            maxPrereqChain = value
+    #result = ""
+    coursecount = {}
+    for c in courses:
+        coursecount[c] = 0
     for key, value in prereqChain.items():
-        for course in value:
-            if course in prereqChain.keys():
-                for prereq in prereqChain[course]:
-                    if prereq not in prereqChain[key]:
-                        change = True
-                        prereqChain[key].append(prereq)
+        for c in value:
+            coursecount[c] = coursecount[c] + 1
 
-maxPrereqChain = 0
-for value in prereqLength.values():
-    if value > maxPrereqChain:
-        maxPrereqChain = value
-result = "";
-for i in range(maxPrereqChain + 1):
-    j = maxPrereqChain - i
-    for key, value in prereqLength.items():
-        if value == j:
-            if key not in complete:
-                print(key, ": ", value)
-                result = result+key+": "+str(value)+"<br>";
-                #for v in prereqChain[key]:
-                    #print("   ", v)
-                    #result=result+"   "+v+"<br>";
-result = result + "<br>";
-print(result)
+    sem = []
+    for c in sorted(coursecount, key=coursecount.get, reverse=True):
+        if len(sem) >= 3:
+            break;
+        if prereqLength[c] == 0:
+            sem.append(c)
+            complete.append(c)
+
+    if (len(sem) == 0):
+        break
+
+    semesters.append(sem)
+    #result = result + "<br>";
+    #print(result)
+    #if result == "<br>":
+    #    break
+for c in courses:
+    if c not in complete:
+        print(c)
+print(semesters)
