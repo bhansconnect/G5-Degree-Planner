7d8a4313dbb5d039f3a4ff46bed1610b404b3cbe
Brendan Hansknecht <Brendan.Hansknecht@gmail.com>
2017-04-09 15:21:37 -0400

    Frontend Integration Bug Fixes

--- Site/pages/DegreePlanner.php
+++ Site/pages/DegreePlanner.php
@@ -61,7 +61,7 @@ require "header.php";
                         <li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
                         </li>
                         <li class="divider"></li>
-                        <li><a href="login.php"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+                        <li><a href="logout.php"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
                         </li>
                     </ul>
                     <!-- /.dropdown-user -->
--- Site/pages/login.php
+++ Site/pages/login.php
@@ -71,10 +71,10 @@ require "header.php";
                         <h3 class="panel-title">Please Sign In</h3>
                     </div>
                     <div class="panel-body">
-                        <form role="form">
+                        <form role="form" method="post" action="login.php">
                             <fieldset>
                                 <div class="form-group">
-                                    <input class="form-control" placeholder="E-mail" name="email" type="email" autofocus>
+                                    <input class="form-control" placeholder="Username" name="username" type="text" autofocus>
                                 </div>
                                 <div class="form-group">
                                     <input class="form-control" placeholder="Password" name="password" type="password" value="">
@@ -85,7 +85,8 @@ require "header.php";
                                     </label>
                                 </div>
                                 <!-- Change this to a button or input when using this as a form -->
-                                <a href="index.php" class="btn btn-lg btn-success btn-block">Login</a>
+                                <!-- a href="login.php" class="btn btn-lg btn-success btn-block">Login</a-->
+								<input class="btn btn-lg btn-success btn-block" type="submit" value="Login">
                             </fieldset>
                         </form>
 						<h4>Need Account?</h4>
